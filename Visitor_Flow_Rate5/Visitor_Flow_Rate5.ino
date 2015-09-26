0c8712bdd1f7b7e1e958f26880c88e6c792b1f55
luooove <644175554@qq.com>
2015-09-26 20:30:18 +0800

    add some
    添加了一些功能：重启后不会清零，上电后会显示进出的人数

--- Visitor_Flow_Rate5/Visitor_Flow_Rate5.ino
+++ Visitor_Flow_Rate5/Visitor_Flow_Rate5.ino
@@ -6,7 +6,7 @@ AT24CXXX ROM_512;
 DS3231 RTC; //Create the DS3231 object
 const int theDeviceAddress = 0x57;
 #include "OLED.h"
-
+#include <MsTimer2.h>               //定时器库的 头文件
 char Director1 = 0;
 int VisitorsIn = 0;
 int VisitorsOut = 0;
@@ -16,7 +16,6 @@ char flag = 0;
 void setup()
 {
   Wire.begin();        //启动I2C通信
-
   LEDPIN_Init();
   LED_Init();
   LED_P6x8Str(20, 0, "Vistor_Flow_Rate:");
@@ -24,50 +23,76 @@ void setup()
   LED_P6x8Str(0, 4, "PeopleOut:");
 
   ROM_512.Memory_Size(512);
- // Serial.begin(9600);
-  count = ROM_512.ReadByte(0x57 ,0) + ROM_512.ReadByte(0x57 ,1)*255;
+  Serial.begin(9600);
+  count = ROM_512.ReadByte(0x57 , 0) + ROM_512.ReadByte(0x57 , 1) * 255;
+  VisitorsIn = ROM_512.ReadByte(0x57 , 2) + ROM_512.ReadByte(0x57 , 3) * 255;
+  VisitorsOut = ROM_512.ReadByte(0x57 , 4) + ROM_512.ReadByte(0x57 , 5) * 255;
+  
+  LED_PrintValueI(60, 2, VisitorsIn);
+  LED_PrintValueI(60, 4, VisitorsOut);
   attachInterrupt(0, Detect1, FALLING  );//下降沿说明有
   attachInterrupt(1, Detect2, FALLING );//下降沿说明人
 }
 
 void Detect1()
 {
-	 sleep_disable();
-	if (Director1 == 0) //means people is go in and not yet pass
-	{
-		Director1 = 1;
-	//	Serial.println("detect1_0");
-	}
+  sleep_disable();
+  if (Director1 == 0) //means people is go in and not yet pass
+  {
+    Director1 = 1;
+    Serial.println("timestart");
+    MsTimer2::set(2000, wait);        // 中断设置函数，每 1s 进入一次中断
+    MsTimer2::start();
+    //	Serial.println("detect1_0");
+  }
   if (Director1 == 2) //means people is going out
   {
-  	Director1 = 0;
-  	VisitorsOut ++;
-  	count++;
-  	flag = 1;
+    Director1 = 0;
+    VisitorsOut ++;
+    count++;
+    flag = 1;
   }
 }
 void Detect2()
 {
-	 sleep_disable();
-	if (Director1 == 0) //means people is go out and not yet pass
-	{
-		Director1 = 2;
-	//	Serial.println("detect2_0");
-	}
-	if (Director1 == 1)
-	{
-		Director1 = 0;
-		VisitorsIn ++;
-		count++;
-		flag = 2;
-	}
+  sleep_disable();
+  if (Director1 == 0) //means people is go out and not yet pass
+  {
+    Director1 = 2;
+    Serial.println("timestart");
+    MsTimer2::set(2000, wait);        // 中断设置函数，每 1s 进入一次中断
+    MsTimer2::start();
+    //	Serial.println("detect2_0");
+  }
+  if (Director1 == 1)
+  {
+    Director1 = 0;
+    VisitorsIn ++;
+    count++;
+    flag = 2;
+  }
 }
 
+void wait()
+{
+  Serial.println("timeout");
+  if ((Director1 == 1))
+  {
+    Director1 = 0;
+    MsTimer2::stop();
+  }
+  if ((Director1 == 2))
+  {
+    Director1 = 0;
+    MsTimer2::stop();
+  }
+  MsTimer2::stop();
+}
 
 
 void SaveData()
 {
-	byte buffer[10];
+  byte buffer[10];
   DateTime now = RTC.now(); //get the current date-time
   buffer[0] = now.year() - 2000;
   buffer[1] = now.month();
@@ -83,17 +108,22 @@ void SaveData()
   //save count
   ROM_512.WriteByte(0x57 , 0 , (byte)count % 255);
   ROM_512.WriteByte(0x57 , 1 , (byte)(count >> 8) % 255);
-
-  ROM_512.WriteByte(0x57 , 2 + count * 10 , buffer[0]);
-  ROM_512.WriteByte(0x57 , 2 + count * 10 + 1 , buffer[1]);
-  ROM_512.WriteByte(0x57 , 2 + count * 10 + 2 , buffer[2]);
-  ROM_512.WriteByte(0x57 , 2 + count * 10 + 3 , buffer[3]);
-  ROM_512.WriteByte(0x57 , 2 + count * 10 + 4 , buffer[4]);
-  ROM_512.WriteByte(0x57 , 2 + count * 10 + 5 , buffer[5]);
-  ROM_512.WriteByte(0x57 , 2 + count * 10 + 6 , buffer[6]);
-  ROM_512.WriteByte(0x57 , 2 + count * 10 + 7 , buffer[7]);
-  ROM_512.WriteByte(0x57 , 2 + count * 10 + 8 , buffer[8]);
-  ROM_512.WriteByte(0x57 , 2 + count * 10 + 9 , buffer[9]);
+  
+  ROM_512.WriteByte(0x57 , 2 , (byte)VisitorsIn % 255);
+  ROM_512.WriteByte(0x57 , 3 , (byte)(VisitorsIn >> 8) % 255);
+  ROM_512.WriteByte(0x57 , 4 , (byte)VisitorsOut % 255);
+  ROM_512.WriteByte(0x57 , 5 , (byte)(VisitorsOut >> 8) % 255);
+  
+  ROM_512.WriteByte(0x57 , 6 + count * 10 , buffer[0]);
+  ROM_512.WriteByte(0x57 , 6 + count * 10 + 1 , buffer[1]);
+  ROM_512.WriteByte(0x57 , 6 + count * 10 + 2 , buffer[2]);
+  ROM_512.WriteByte(0x57 , 6 + count * 10 + 3 , buffer[3]);
+  ROM_512.WriteByte(0x57 , 6 + count * 10 + 4 , buffer[4]);
+  ROM_512.WriteByte(0x57 , 6 + count * 10 + 5 , buffer[5]);
+  ROM_512.WriteByte(0x57 , 6 + count * 10 + 6 , buffer[6]);
+  ROM_512.WriteByte(0x57 , 6 + count * 10 + 7 , buffer[7]);
+  ROM_512.WriteByte(0x57 , 6 + count * 10 + 8 , buffer[8]);
+  ROM_512.WriteByte(0x57 , 6 + count * 10 + 9 , buffer[9]);
 
 
 
@@ -103,84 +133,85 @@ void SaveData()
 
 
 void printSaveData()
-{	byte buffer[10];
+{ byte buffer[10];
 
-	buffer[0] = ROM_512.ReadByte(0x57 ,2 + count * 10);
-	buffer[1] = ROM_512.ReadByte(0x57 ,2 + count * 10 + 1);
-	buffer[2] = ROM_512.ReadByte(0x57 ,2 + count * 10 + 2);
-	buffer[3] = ROM_512.ReadByte(0x57 ,2 + count * 10 + 3);
-	buffer[4] = ROM_512.ReadByte(0x57 ,2 + count * 10 + 4);
-	buffer[5] = ROM_512.ReadByte(0x57 ,2 + count * 10 + 5);
+  buffer[0] = ROM_512.ReadByte(0x57 , 6 + count * 10);
+  buffer[1] = ROM_512.ReadByte(0x57 , 6 + count * 10 + 1);
+  buffer[2] = ROM_512.ReadByte(0x57 , 6 + count * 10 + 2);
+  buffer[3] = ROM_512.ReadByte(0x57 , 6 + count * 10 + 3);
+  buffer[4] = ROM_512.ReadByte(0x57 , 6 + count * 10 + 4);
+  buffer[5] = ROM_512.ReadByte(0x57 , 6 + count * 10 + 5);
 
-	buffer[6] = ROM_512.ReadByte(0x57 ,2 + count * 10 + 6);
-	buffer[7] = ROM_512.ReadByte(0x57 ,2 + count * 10 + 7);
+  buffer[6] = ROM_512.ReadByte(0x57 , 6 + count * 10 + 6);
+  buffer[7] = ROM_512.ReadByte(0x57 , 6 + count * 10 + 7);
 
-	buffer[8] = ROM_512.ReadByte(0x57 ,2 + count * 10 + 8);
-	buffer[9] = ROM_512.ReadByte(0x57 ,2 + count * 10 + 9);
+  buffer[8] = ROM_512.ReadByte(0x57 , 6 + count * 10 + 8);
+  buffer[9] = ROM_512.ReadByte(0x57 , 6 + count * 10 + 9);
 
-	Serial.println((ROM_512.ReadByte(0x57 ,0))+((ROM_512.ReadByte(0x57 ,1))<<8),DEC);
+  Serial.println((ROM_512.ReadByte(0x57 , 0)) + ((ROM_512.ReadByte(0x57 , 1)) << 8), DEC);
 
-    Serial.print(2000 + buffer[0], DEC);  //year
-    Serial.print('-');
-    Serial.print(buffer[1], DEC);//month
-    Serial.print('-');
-    Serial.print(buffer[2], DEC);//day
-    Serial.print('/');
+  Serial.print(2000 + buffer[0], DEC);  //year
+  Serial.print('-');
+  Serial.print(buffer[1], DEC);//month
+  Serial.print('-');
+  Serial.print(buffer[2], DEC);//day
+  Serial.print('/');
 
-    Serial.print(buffer[3], DEC);//hour
-    Serial.print(':');
-    Serial.print(buffer[4], DEC);//min
-    Serial.print(':');
-    Serial.print(buffer[5], DEC);//sec
-    Serial.print('#');
+  Serial.print(buffer[3], DEC);//hour
+  Serial.print(':');
+  Serial.print(buffer[4], DEC);//min
+  Serial.print(':');
+  Serial.print(buffer[5], DEC);//sec
+  Serial.print('#');
 
-    int temp = buffer[6] + buffer[7] * 255;
-    Serial.print(temp, DEC);
-    Serial.print('/');
-    int temp2 = buffer[8] + buffer[9] * 255;
-    Serial.print(temp2, DEC);
-    Serial.println();
-    delay(10);
+  int temp = buffer[6] + buffer[7] * 255;
+  Serial.print(temp, DEC);
+  Serial.print('/');
+  int temp2 = buffer[8] + buffer[9] * 255;
+  Serial.print(temp2, DEC);
+  Serial.println();
+  delay(10);
 
 }
 void loop()
 {
+  
 
-	switch (flag) {
-		case 1:
+  switch (flag) {
+    case 1:
 
       // do something
       SaveData();
-   //   printSaveData();
+      //   printSaveData();
       flag = 0;
       break;
-      case 2:
+    case 2:
       // do something
       SaveData();
-   //   printSaveData();
+      //   printSaveData();
       flag = 0;
       break;
-      default:;
+    default:;
       // do something
   }
-  // disable ADC
-  ADCSRA = 0;  
-  
-  set_sleep_mode (SLEEP_MODE_PWR_DOWN);  
-  sleep_enable();
+  //   disable ADC
+  //  ADCSRA = 0;
+
+  //  set_sleep_mode (SLEEP_MODE_PWR_DOWN);
+  //  sleep_enable();
 
   // Do not interrupt before we go to sleep, or the
   // ISR will detach interrupts and we won't wake.
-  noInterrupts ();
-    // turn off brown-out enable in software
+  // noInterrupts ();
+  // turn off brown-out enable in software
   // BODS must be set to one and BODSE must be set to zero within four clock cycles
-  MCUCR = bit (BODS) | bit (BODSE);
-  // The BODS bit is automatically cleared after three clock cycles
-  MCUCR = bit (BODS); 
-  
+  //  MCUCR = bit (BODS) | bit (BODSE);
+  //  // The BODS bit is automatically cleared after three clock cycles
+  //  MCUCR = bit (BODS);
+
   // We are guaranteed that the sleep_cpu call will be done
   // as the processor executes the next instruction after
   // interrupts are turned on.
   interrupts ();  // one cycle
-  sleep_cpu ();   // one cycle
+  //  sleep_cpu ();   // one cycle
 }
