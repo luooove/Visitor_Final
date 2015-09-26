82895996af7fc517b6aa234c5b55832cadfa3144
luooove <644175554@qq.com>
2015-09-26 21:32:43 +0800

    add clear
    添加新功能：清零功能，因为arduino会进入睡眠模式，因此无法用循环检测的方式来检测按键。正确的方式：连接清零端口，或者按键后出发一次光电开光正常工作。然后断开。

--- Visitor_Flow_Rate5/Visitor_Flow_Rate5.ino
+++ Visitor_Flow_Rate5/Visitor_Flow_Rate5.ino
@@ -12,28 +12,36 @@ int VisitorsIn = 0;
 int VisitorsOut = 0;
 int count = 0;
 char flag = 0;
-
+const int buttonPin = 4;     // the number of the pushbutton pin
+int buttonState = 0;         // variable for reading the pushbutton status
+//-----------------------初始化------------------------------
 void setup()
 {
+  pinMode(buttonPin, INPUT); //
+
   Wire.begin();        //启动I2C通信
-  LEDPIN_Init();
+  LEDPIN_Init();       //初始化液晶
   LED_Init();
   LED_P6x8Str(20, 0, "Vistor_Flow_Rate:");
   LED_P6x8Str(0, 2, "PeopleIn:");
   LED_P6x8Str(0, 4, "PeopleOut:");
 
-  ROM_512.Memory_Size(512);
-  Serial.begin(9600);
+  ROM_512.Memory_Size(512);     //例化存储器
+  Serial.begin(9600);           //初始化串口
+  //读取外部存储器中的总人数，进去人数和出来人数
   count = ROM_512.ReadByte(0x57 , 0) + ROM_512.ReadByte(0x57 , 1) * 255;
   VisitorsIn = ROM_512.ReadByte(0x57 , 2) + ROM_512.ReadByte(0x57 , 3) * 255;
   VisitorsOut = ROM_512.ReadByte(0x57 , 4) + ROM_512.ReadByte(0x57 , 5) * 255;
-  
+  //把进去和出来的人数显示
   LED_PrintValueI(60, 2, VisitorsIn);
   LED_PrintValueI(60, 4, VisitorsOut);
+  //使能外部中断
   attachInterrupt(0, Detect1, FALLING  );//下降沿说明有
   attachInterrupt(1, Detect2, FALLING );//下降沿说明人
 }
 
+
+//-----------------------中断服务程序，检测人进出------------------------------
 void Detect1()
 {
   sleep_disable();
@@ -72,7 +80,7 @@ void Detect2()
     flag = 2;
   }
 }
-
+//-----------------------定时器中断服务程序，如果2秒内人还未通过就清除标志------------------------------
 void wait()
 {
   Serial.println("timeout");
@@ -89,7 +97,8 @@ void wait()
   MsTimer2::stop();
 }
 
-
+//-----------------------保存数据，并显示在ＯＬＥＤ上------------------------------
+//-----------------------保存的格式为：年 月 日，进入人数，出来人数------------------------------
 void SaveData()
 {
   byte buffer[10];
@@ -108,12 +117,12 @@ void SaveData()
   //save count
   ROM_512.WriteByte(0x57 , 0 , (byte)count % 255);
   ROM_512.WriteByte(0x57 , 1 , (byte)(count >> 8) % 255);
-  
+
   ROM_512.WriteByte(0x57 , 2 , (byte)VisitorsIn % 255);
   ROM_512.WriteByte(0x57 , 3 , (byte)(VisitorsIn >> 8) % 255);
   ROM_512.WriteByte(0x57 , 4 , (byte)VisitorsOut % 255);
   ROM_512.WriteByte(0x57 , 5 , (byte)(VisitorsOut >> 8) % 255);
-  
+
   ROM_512.WriteByte(0x57 , 6 + count * 10 , buffer[0]);
   ROM_512.WriteByte(0x57 , 6 + count * 10 + 1 , buffer[1]);
   ROM_512.WriteByte(0x57 , 6 + count * 10 + 2 , buffer[2]);
@@ -131,7 +140,7 @@ void SaveData()
   LED_PrintValueI(60, 4, VisitorsOut);
 }
 
-
+//-----------------------读取数据并打印------------------------------
 void printSaveData()
 { byte buffer[10];
 
@@ -173,10 +182,22 @@ void printSaveData()
   delay(10);
 
 }
+void ClearData()
+{
+  ROM_512.WriteByte(0x57 , 0 , 0);
+  ROM_512.WriteByte(0x57 , 1 , 0);
+
+  ROM_512.WriteByte(0x57 , 2 , 0);
+  ROM_512.WriteByte(0x57 , 3 , 0);
+  ROM_512.WriteByte(0x57 , 4 , 0);
+  ROM_512.WriteByte(0x57 , 5 , 0);
+}
+
+//-----------------------主循环------------------------------
 void loop()
 {
-  
 
+  //主函数，因为把服务程序放在中断服务程序中处理会出错，因此通过flag的方式来进行中断服务程序的处理。
   switch (flag) {
     case 1:
 
@@ -194,24 +215,29 @@ void loop()
     default:;
       // do something
   }
-  //   disable ADC
-  //  ADCSRA = 0;
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == LOW) {
+    ClearData();
+  }
 
-  //  set_sleep_mode (SLEEP_MODE_PWR_DOWN);
-  //  sleep_enable();
+  //   disable ADC
+  ADCSRA = 0;
+
+  set_sleep_mode (SLEEP_MODE_PWR_DOWN);
+  sleep_enable();
 
   // Do not interrupt before we go to sleep, or the
   // ISR will detach interrupts and we won't wake.
-  // noInterrupts ();
+  noInterrupts ();
   // turn off brown-out enable in software
   // BODS must be set to one and BODSE must be set to zero within four clock cycles
-  //  MCUCR = bit (BODS) | bit (BODSE);
+  MCUCR = bit (BODS) | bit (BODSE);
   //  // The BODS bit is automatically cleared after three clock cycles
-  //  MCUCR = bit (BODS);
+  MCUCR = bit (BODS);
 
   // We are guaranteed that the sleep_cpu call will be done
   // as the processor executes the next instruction after
   // interrupts are turned on.
   interrupts ();  // one cycle
-  //  sleep_cpu ();   // one cycle
+  sleep_cpu ();   // one cycle
 }
